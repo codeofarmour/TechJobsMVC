e800385c49669f657b1107979fe87e1b5c265edd
Terri Riggle <codeofarmour@outlook.com>
2018-02-24 21:26:27 -0500

    Created a Results action method to process search requests and display results

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -16,5 +16,16 @@ namespace TechJobs.Controllers
         // TODO #1 - Create a Results action method to process 
         // search request and display results
 
+        public IActionResult Results(string searchTerm, string searchType)
+        {
+            List<Dictionary<string, string>> jobs = new List<Dictionary<string, string>>();
+            ViewBag.columns = ListController.columnChoices;
+
+            if (searchType == "all")
+            {
+                jobs = JobData.FindByValue(searchTerm);
+            }
+        }
+
     }
 }
